96bad8ef266b067b29cf8764cd7ea8a5d028cf05
gui <guillaume.lay@iltr.fr>
2019-12-05 14:30:08 +0100

    Day 2 premier partie

--- index.js
+++ index.js
@@ -2,24 +2,31 @@ var fs = require('fs');
 
 var fileName = 'input.txt';
 var file = fs.readFileSync(fileName);
-var input = file.toString().split('\r\n');
+var input = file.toString().split(',');
 //console.log(input);
 
-var total = 0;
-input.forEach(function(chiffre){
-	var value = parseInt(chiffre,10);
-	if(!isNaN(value)){
-		value = calculate(value);
-		while(value>0){
-			total+=value;
-			value=calculate(value);
-		}
-		console.log(value);
-		
+var i=0;
+var stop=false;
+while(i < input.length && !stop){
+	var operator = parseInt(input[i],10);
+	var val1 = parseInt(input[i+1],10);
+	var val2 = parseInt(input[i+2],10);
+	var result = 0;
+	
+	if(operator == 1){
+		result = parseInt(input[val1],10) + parseInt(input[val2],10);
+		input[parseInt(input[i+3],10)]=result;
+	}else if(operator == 2){
+		result = parseInt(input[val1],10)*parseInt(input[val2],10);
+		input[parseInt(input[i+3],10)]=result;
+	} else{
+		stop = true;
 	}
-});
-console.log(total);
-
-function calculate(mass){
-	return Math.floor(mass / 3) -2;
+	console.log(input[val1] +" "+operator+" "+input[val2]+" = "+result);
+	
+	
+	
+	i+=4;
 }
+
+console.log(input[0]);
