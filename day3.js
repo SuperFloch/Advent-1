e9a16738467b169de3f965a615ca3de204a5a9f3
gui <guillaume.lay@iltr.fr>
2019-12-06 18:09:29 +0100

    day3

---
+++ day3.js
@@ -0,0 +1,89 @@
+var fs = require('fs');
+
+calculate();
+
+
+function calculate(){
+	
+	var fileName = 'input.txt';
+	var file = fs.readFileSync(fileName);
+	var path1 = file.toString().split('\r\n')[0];
+	var path2 = file.toString().split('\r\n')[1];
+	
+	var intersections=[];
+	
+	var central=new Point(0,0);
+	
+	var path1Points=cutPath(path1);
+	var path2Points=cutPath(path2);
+	
+	var commonPoints= getCommonPoints(path1Points,path2Points);
+	console.log(commonPoints);
+	var dists = [];
+	commonPoints.forEach(function(cp){
+		var dist = Math.abs(cp.x)+ Math.abs(cp.y);
+		dists.push(dist);
+	});
+	console.log(dists.sort());
+	
+}
+
+function getCommonPoints(path1,path2){
+	var ret = [];
+	
+	path1.forEach(function(point){
+		path2.forEach(function(point2){
+			if(point.x == point2.x && point.y == point2.y){
+				ret.push(point);
+			}
+		});
+	});
+	return ret;
+}
+
+function cutPath(path){
+	var instructions=path.split(',');
+	var currentPoint=new Point(0,0);
+	
+	var pointPath=[];
+	
+	instructions.forEach(function(instr){
+		var direction = instr.charAt(0);
+		var value = parseInt(instr.substring(1,4),10);
+		switch(direction){
+			case 'U':
+			for(var i=0;i<value;i++){
+				currentPoint.y++;
+				pointPath.push(new Point(currentPoint.x,currentPoint.y));
+			}
+			break;
+			
+			case 'R':
+			for(var i=0;i<value;i++){
+				currentPoint.x++;
+				pointPath.push(new Point(currentPoint.x,currentPoint.y));
+			}
+			break;
+			
+			case 'L':
+			for(var i=0;i<value;i++){
+				currentPoint.x--;
+				pointPath.push(new Point(currentPoint.x,currentPoint.y));
+			}
+			break;
+			
+			case 'D':
+			for(var i=0;i<value;i++){
+				currentPoint.y--;
+				pointPath.push(new Point(currentPoint.x,currentPoint.y));
+			}
+			break;
+		}
+	});
+	return pointPath;
+}
+
+function Point(x,y){
+	this.x=x;
+	this.y=y;
+}
